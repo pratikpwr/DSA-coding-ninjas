e21f32036136ef514c5df17ca301866f5d25fa25
pratikpwr <pratiksatishpawar@gmail.com>
2021-07-21 22:14:59 +0530

    pointer arithmatic

--- Level 1/pointers/pointer_arithmatic.cpp
+++ Level 1/pointers/pointer_arithmatic.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+// Pointer arithmatic
 int main() {
 
     int i = 10;
@@ -12,6 +13,8 @@ int main() {
     // i.e. adds 4 bits to address
     p=p+1;
     cout<<p<<endl;
+
+    // this arimatic of array mostly works in the case of arrays
 
     return 0;
 }
